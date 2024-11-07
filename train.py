19b42910d62981cb11ce1fd2c652640e520d0d09
lav7n <lavensri@gmail.com>
2024-11-08 01:47:59 +0530

    .

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ parser.add_argument('--num_epochs', type=int, default=5, help='Number of epochs 
 parser.add_argument('--FLTrust', action='store_true', help='Use FLTrust or not')
 parser.add_argument('--fedprox', action='store_true', help='Use FedProx or not')
 parser.add_argument('--attack_type', type=str, default='gaussian', help='Type of attack to apply to malicious clients')
-parser.add_argument('--noise_stddev', type=float, default=256, help='Standard deviation of noise for Gaussian noise attack')
+parser.add_argument('--noise_stddev', type=float, default=64, help='Standard deviation of noise for Gaussian noise attack')
 parser.add_argument('--printmetrics', action='store_true', help='Print metrics or not')
 parser.add_argument('--lr', type=float, default=0.001, help='Learning rate for clients')
 parser.add_argument('--distribution', type=str, default='non_iid', help='Data distribution among clients')
